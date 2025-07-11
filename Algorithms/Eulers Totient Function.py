01d5bda327db408eea24df4fcbc22ee2c2946cc8
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-07-11 23:38:04 +0100

    Update Eulers Totient Function.py

--- Algorithms/Eulers Totient Function.py
+++ Algorithms/Eulers Totient Function.py
@@ -1,17 +1,17 @@
 #27/10/2024 Implementation of Eulers totient function
 
-def highestCommonDivisor(a,b):
+def highest_common_divisor(a,b):
     while a%b!=0:
         a,b=b,a%b
     return(b)
 
 def isCoprime(a,b):
-    return(highestCommonDivisor(a,b)==1)
+    return(highest_common_divisor(a,b)==1)
 
 def totient(n):
     value=0
     for i in range(1,n):
-        if isCoprime(i,n):
+        if is_coprime(i,n):
             value+=1
     return(value)
 
