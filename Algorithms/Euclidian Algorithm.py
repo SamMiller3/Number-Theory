b75a9003d13625f8a17506a48ae52b4857b8fb72
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-07-11 23:39:14 +0100

    Update Euclidian Algorithm.py

--- Algorithms/Euclidian Algorithm.py
+++ Algorithms/Euclidian Algorithm.py
@@ -1,7 +1,7 @@
 #27/10/2024 Euclidian Algorithm
-firstNumber=int(input("Enter first integer: "))
-secondNumber=int(input("Enter second integer: "))
-a,b=max(firstNumber,secondNumber),min(firstNumber,secondNumber)
+first_number=int(input("Enter first integer: "))
+second_number=int(input("Enter second integer: "))
+a,b=max(first_number,second_number),min(first_number,second_number)
 while a%b!=0:
     a,b=b,a%b
 print(b)
