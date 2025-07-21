e0d0af5bd798318b6a421c75bdea725086c5f772
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-07-22 00:07:55 +0100

    Add files via upload

---
+++ Algorithms/linear congruence equation solver.py
@@ -0,0 +1,33 @@
+# solve linear congruence equations of the form ax=b(mod m)
+# 21/07/2025
+
+def extended_gcd(a,b): # extended euclidean algorithm
+    if a % b == 0:
+        return (b, 0, 1) 
+    quotient = a//b 
+    remainder = a - quotient * b 
+    gcd, old_x , old_y = extended_gcd(b, remainder)
+    x = old_y
+    y = old_x - old_y * quotient
+    return gcd, x, y
+
+print("Input format: ax = b (mod m) give integers a, b and m.")
+print("For example for 75x = 12 (mod 237) x = 76, 155, 234 (mod 237).")
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+m = int(input("enter m: "))
+gcd,p,q = extended_gcd(a,m) # use extended euclidean algorithm to solve Bezout's identity
+if b % gcd != 0: # solutions exist if and only if d divides m
+    print("No solutions exist to the given equation.")
+elif gcd == 1: # if a and m are coprime find multiplicative inverse
+    inverse = p
+    x = (b * inverse) % m
+    print(f"x = {x} (mod {m}).")
+else: # otherwise there are d solutions modulo m
+    k = p * (b//gcd)
+    increment = m//gcd
+    solutions=[]
+    for i in range(gcd):
+        solutions.append(str(k))
+        k += increment
+    print(f"x = {', '.join(solutions)}. (mod {m})")
